8594242d5671b704e8c30557a618784a2b7bd31d
KsinghP <49181830+KsinghP@users.noreply.github.com>
2019-06-10 21:08:00 -0600

    Update Code to scrape data.py

--- Code to scrape data.py
+++ Code to scrape data.py
@@ -29,7 +29,7 @@ code_of_state = ['01', '02', '03', '04', '05', '06', '07', '08', '10', '11', '12
 state_ut = ['S', 'U']
 
 #this method scrapes three variables - number of candidates, total votes polled and name of constituency - for each constituency
-def scrape1():
+def scrape_name_candidates_votes():
     #declaring empty lists for each of the variables to be scraped
     #the first variable to be scraped is name of constituency
     name_of_const = []
@@ -46,42 +46,51 @@ def scrape1():
     for j in (code_of_state):
         for i in (page_number):
             for k in (state_ut):  
+                #the variable page goes to each URL
                 page = requests.get('http://results.eci.gov.in/pc/en/constituencywise/Constituencywise' + k + j + i +'.htm')
                 soup = BeautifulSoup(page.content, 'html.parser')
+                #the next three lines of code scrape number of candidates in each constituency
+                #full_table scrapes the HTML code for each row except the last; each row is depicted by the tr tag "font-size:12px;" 
+                #the first column of each row contains the serial number of candidate; number of candidates is equal to 
+                #the final serial number, which is equal to length of full_table
                 full_table = soup.find_all(attrs={"style":"font-size:12px;"})
+                #the if condition below is to deal with the fact that full_table will take null values for all URLs that don't exist
+                #for example, the URL with code_of_state = 02 and page_number = 3 does not exist
                 if (len(full_table) is not 0):
                     number_of_candidates.append(len(full_table))
+                # the next 4 lines scrape total votes polled in each constituency
                 vote_row = soup.find(attrs={"style":"color: #fff; background: #105980; border-color:#673033; border-width:1px;border-style:Solid;font-family:Calibri;"})
                 if (vote_row is not None):
                         td_list = vote_row.find_all("td")
                         total_votes.append(td_list[5].get_text())
                 name_row = soup.find(class_="table-party")
+                #the if condition used below is used to deal with the issue of null values, as done above
                 if (name_row is not None):
                     tentative1_name_of_const.append(name_row.find("tr").get_text())
+                    # the name is split on "-" because it's initially scraped as <Name of State-Name of Constituency>
                     tentative2_name_of_const.append(tentative1_name_of_const[-1].strip().split("-"))     
+    #the tuple is used to extract Name of Constituency after splitting on "-"
     temporary_tuple = tuple(tentative2_name_of_const)
     name_of_const = [t[1] for t in temporary_tuple]
-    combined_list = list(zip(name_of_const, number_of_candidates, total_votes))
-    df_const_candidates_votes = pd.DataFrame(np.array(combined_list), columns = list("abc"))
-    df_const_candidates_votes.columns = ['Constituency', 'Candidates', 'Votes']
-    df_const_candidates_votes['Candidates'] = pd.to_numeric(df_const_candidates_votes['Candidates'])
-    df_const_candidates_votes['Votes'] = pd.to_numeric(df_const_candidates_votes['Votes'])
-    print(df_const_candidates_votes)
+    combined_name_candidates_votes = list(zip(name_of_const, number_of_candidates, total_votes))
+    #print(combined_name_candidates_votes)
+    return (combined_name_candidates_votes)
 
+#this function writes name of constituency, number of candidates and total votes polled to a CSV file
+def write_first_csv_file():
+    combined_name_candidates_votes = scrape_name_candidates_votes()
+    
     with open('const_candidates_votes.csv', 'w') as csvFile:
        w = csv.writer(csvFile)
        w.writerow(['Constituency', 'Candidates', 'Votes'])
-       for data in combined_list:
+       for data in combined_name_candidates_votes:
            if data:
                w.writerow(data)
-
-scrape1()
-
-
+write_first_csv_file()
 
 #this method scrapes three variables - margin of victory of winning candidate, party of winning candidate and name of constituency - for each constituency
 
-def scrape2():
+def scrape_name_margin_party():
     name_of_const = []
     margin = []
     party_name = []
@@ -97,23 +106,18 @@ def scrape2():
                         margin.append(td.find(attrs={"align":"right"}).get_text())
                         party_name.append(td.find("tbody").find("td").get_text())
             
-    combined = list(zip(name_of_const, margin, party_name))
-    df_const_margin_party = pd.DataFrame(np.array(combined), columns = list("abc"))
-    df_const_margin_party.columns = ['Constituency', 'Margin', 'Party']
-    df_const_margin_party['Margin'] = pd.to_numeric(df_const_margin_party['Margin'])
-    #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    #    print(df_const_margin_party)
-    #print(df_const_margin_party)        
-   
-    
-    
-    
+    combined_name_margin_party = list(zip(name_of_const, margin, party_name))
+    #print(combined_name_margin_party)        
+    return(combined_name_margin_party)
+
+#this function writes name of constituency, margin of victory and party of winning candidate to a CSV file
+def write_second_csv_file():
+    combined_name_margin_party = scrape_name_margin_party
     with open('const_margin_party.csv', 'w') as csvFile:
        w = csv.writer(csvFile)
        w.writerow(['Constituency', 'Margin', 'Party'])
-       for data in combined:
+       for data in combined_name_margin_party:
            if data:
                w.writerow(data)
     
-    
-scrape2()    
+write_second_csv_file    
