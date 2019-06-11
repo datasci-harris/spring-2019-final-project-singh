a3bad9f20a06e3cf743fc9aa438a0876e6ad178d
KsinghP <49181830+KsinghP@users.noreply.github.com>
2019-06-10 20:03:27 -0600

    Update Code for working on scraped CSV.py

--- Code for working on scraped CSV.py
+++ Code for working on scraped CSV.py
@@ -11,26 +11,29 @@ import statsmodels.api as sm
 from sklearn import linear_model
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from pandas.tools.plotting import table
 
+#this function reads the CSV files created through scraping
 def data_read():
     const_candidates_votes = pd.read_csv("const_candidates_votes.csv")
     const_margin_party = pd.read_csv("const_margin_party.csv")
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(const_candidates_votes)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(const_margin_party)
     return const_candidates_votes, const_margin_party
 
+#this function merges the above two dataframes
 def data_merge():
     const_candidates_votes, const_margin_party = data_read()
     const_margin_party['Constituency'] = const_margin_party['Constituency'].str.upper()
     const_candidates_votes['Constituency'] = const_candidates_votes['Constituency'].str.upper()
-    print(const_margin_party['Constituency'])
-    print(const_candidates_votes['Constituency'])
 
     election_data_merged = pd.merge(const_candidates_votes, const_margin_party, on='Constituency')
-    election_data_merged['Margin Percentage'] = (election_data_merged['Margin']/election_data_merged['Votes'])*100
     print(election_data_merged.head())      
     return(election_data_merged)
 
-
+#this function does OLS modelling; dependent variable is Margin in each constituency, and independent variables are 
+#number of canadidates and votes polled in each constituency
 def regression():        
     election_data_merged = data_merge()
     x = election_data_merged[['Candidates', 'Votes']]
@@ -39,21 +42,24 @@ def regression():
     model_election_data = sm.OLS(y, x).fit()
     print(model_election_data.summary())
 
-#regression()
+regression()
 
-
+#this function merges the merged dataframe with the shapefile on the columns "Constituency" and "PC_NAME" 
+#it then plots two charts for the merged shapefile
 def chloropleth():
     election_data_merged = data_merge()
     india_shapefile = gpd.read_file("india.shp")
-#india_shapefile.plot()
+    # creating a new column to normalise margin of victory by total votes polled in the constituency
+    election_data_merged['Margin Percentage'] = (election_data_merged['Margin']/election_data_merged['Votes'])*100
     data_shapefile_merged = india_shapefile.merge(election_data_merged, right_on="Constituency", left_on="PC_NAME", how="inner")
     plot1 = 'Margin'
     plot2 = 'Margin Percentage'
     data_shapefile_merged.plot(plot1, cmap='Reds', legend=True, title = 'Absolute Margin ')
     data_shapefile_merged.plot(plot2, cmap='Reds', legend=True, title = 'Margin as % of votes polled')
 
-#chloropleth()
+chloropleth()
 
+#this function is used to draw a pie chart and a bar chart
 def charts():
     mean_margin_by_party = election_data_merged.groupby(['Party']).mean()
     seats_by_party = election_data_merged.groupby(['Party']).count()
